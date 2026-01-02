ac3e768a37fc604622f8ea7b2d8b0996bd9f8c3a
hfa0555916989 <hfa.0555916989@gmail.com>
2026-01-02 10:20:41 +0300

    Fix: resolve TS type errors in licensing + first-run bootstrap

---
+++ src/vs/platform/licensing/common/licensing.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const LICENSE_BASE_URL = 'https://license.hassanide.local';
+
+export const LICENSE_ENDPOINTS = {
+	activate: '/activate',
+	status: '/status',
+	refresh: '/refresh'
+};
